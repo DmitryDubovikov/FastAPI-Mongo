a3c69780f454c857b24049bc9885f9046c61fda9
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-11-16 07:31:40 -0300

    add improvements to init_db.py

--- init_db.py
+++ init_db.py
@@ -1,21 +1,23 @@
-from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import MongoClient
 
-mongo_client = AsyncIOMotorClient("mongodb://root:example@mongo:27017/")
-db = mongo_client["form_templates"]
+MONGO_URI = "mongodb://root:example@mongo:27017/"
+DB_NAME = "form_templates"
+COLLECTION_NAME = "form_templates"
 
-db.form_templates.drop()
-
-# Пример шаблона формы
 form_template = {
     "name": "user_email + user_phone",
     "user_email": "email",
     "user_phone": "phone",
 }
-
 form_template2 = {"name": "user_name only", "user_name": "text"}
 form_template3 = {"name": "date only", "important_date": "date"}
 
-# Добавление шаблона формы в базу данных
-db.form_templates.insert_one(form_template)
-db.form_templates.insert_one(form_template2)
-db.form_templates.insert_one(form_template3)
+mongo_client = MongoClient(MONGO_URI)
+db = mongo_client[DB_NAME]
+
+if COLLECTION_NAME in db.list_collection_names():
+    db[COLLECTION_NAME].drop()
+
+db[COLLECTION_NAME].insert_one(form_template)
+db[COLLECTION_NAME].insert_one(form_template2)
+db[COLLECTION_NAME].insert_one(form_template3)
